b71e0421ec343f4dad14427beaae864e9b14b3c4
Nick Sandstrom <32273437+nick4810@users.noreply.github.com>
2026-06-28 20:52:31 -0700

    Added init queries

---
+++ resolvers.js
@@ -0,0 +1,15 @@
+import db from './_db.js';
+
+export const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        reviews() {
+            return db.reviews;
+        },
+        authors() {
+            return db.authors;
+        },
+    }
+}
