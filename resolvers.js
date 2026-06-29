17e1526dd99cd1dc2ca76ffb5e42779d64476442
Nick Sandstrom <32273437+nick4810@users.noreply.github.com>
2026-06-28 21:15:54 -0700

    Added relationship queries

--- resolvers.js
+++ resolvers.js
@@ -20,5 +20,23 @@ export const resolvers = {
         author(_, args) {
             return db.authors.find((author) => author.id === args.id);
         }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.gameId === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.authorId === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((author) => author.id === parent.authorId);
+        },
+        game(parent) {
+            return db.games.find((game) => game.id === parent.gameId);
+        }
     }
 }
